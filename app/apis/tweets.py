2f7c463a166bc47adc3f8ec6c87312e20e74f595
Octave benil <asetoben@gmail.com>
2021-12-21 16:47:46 +0300

    custom tweet

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -1,9 +1,28 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
+from app.db import tweet_repository
+from flask import json
 
 api = Namespace("tweets", description="Tous ce qui concerne les tweets")
 
+model = api.model("Model", {
+    "id": fields.Integer,
+    "text": fields.String,
+    "created_at": fields.DateTime
+})
 
-@api.route("/hello")
+class TweetDao(object):
+    def __init__(self, id, text, created_at):
+        self.id = id
+        self.text = text
+        self.created_at = created_at
+
+@api.route('/<int:id>')
+@api.response(404, 'Tweet not found')
 class TweetResource(Resource):
-    def get(self):
-        return "Goodbye. See you next time."
+    @api.marshal_with(model)
+    def get(self, id):
+        tweet = tweet_repository.get(id)
+        if tweet is None:
+            api.abort(404)
+        else:
+            return TweetDao(id=tweet.id, text=tweet.text, created_at=tweet.created_at)
--- tests/apis/test_tweet_view.py
+++ tests/apis/test_tweet_view.py
@@ -1,7 +1,8 @@
 from flask_testing import TestCase
 from app import create_app
 
-from app.models import Tweet
+from app.models.tweet import Tweet
+from app.db import tweet_repository
 
 class TestTweetView(TestCase):
     def create_app(self):
@@ -9,9 +10,20 @@ class TestTweetView(TestCase):
         app.config['TESTING'] = True
         return app
 
-    def test_tweet(self):
-        response = self.client.get("/tweets/hello")
-        text = response.data.decode()
-        print(text)
-        self.assertIn("Goodbye", text)
+    def setUp(self):
+        tweet_repository.clear()
+
+    def test_tweet_show(self):
+
+        first_tweet = Tweet("First tweet")
+        tweet_repository.add(first_tweet)
+
+        response = self.client.get("/tweets/1")
+        response_tweet = response.json
+
+        print(response_tweet)
         
+        self.assertEqual(response_tweet["id"], 1)
+        self.assertEqual(response_tweet["text"], "First tweet")
+        self.assertIsNotNone(response_tweet["created_at"])
+
